7b85597b01c727e566f3c974de98841abddd0e83
t.aponte <t.aponte@uniandes.edu.co>
2025-03-13 14:46:26 -0500

    al

---
+++ DataStructures/Map/map_linear_probing.py
@@ -0,0 +1,11 @@
+import map_functions as mf
+import array_list form list as al
+import math as m
+
+def new_map(num_elements, load_factor, prime=109345121):
+    capacity = mf.next_prime(num_elements/load_factor)
+    scale = m.randint(1, prime - 1)
+    shift = m.randint(0, prime - 1)
+    table = al.
+    return new_map
+
