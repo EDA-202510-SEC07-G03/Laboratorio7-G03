1d8f58fd801241a8da0f99a4fbd4591fb0182867
t.aponte <t.aponte@uniandes.edu.co>
2025-03-13 15:23:19 -0500

    funcion_new_map

--- DataStructures/Map/map_linear_probing.py
+++ DataStructures/Map/map_linear_probing.py
@@ -1,11 +1,18 @@
-import map_functions as mf
-import array_list form list as al
-import math as m
+from DataStructures.List import array_list as al
+import DataStructures.Map.map_functions as map
+import DataStructures.Map.map_entry as ent
+import random as m
 
 def new_map(num_elements, load_factor, prime=109345121):
-    capacity = mf.next_prime(num_elements/load_factor)
-    scale = m.randint(1, prime - 1)
-    shift = m.randint(0, prime - 1)
-    table = al.
-    return new_map
-
+    my_table =  al.new_list()
+    my_table["prime"] = prime
+    my_table["capacity"] = map.next_prime(num_elements/load_factor)
+    my_table["scale"] = m.randint(1, prime - 1)
+    my_table["shift"] = m.randint(0, prime - 1)
+    my_table["table"] = al.new_list()
+    for i in range(my_table["capacity"]):
+        al.add_last(my_table["table"],ent.new_map_entry(None,None))
+        my_table["table"]["size"] += 1
+    my_table["current_factor"] = 0
+    my_table["limit_factor"] =  load_factor
+    return my_table
